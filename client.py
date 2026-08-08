8523dc125feb9b35d5e6a688dd3f94c3b015de45
Ruslan <RuslanFedorov303@gmail.com>
2026-08-08 17:27:15 +0300

    Update client.py

--- client.py
+++ client.py
@@ -6,9 +6,11 @@ from threading import Thread
 # ---ПУГАМЕ НАЛАШТУВАННЯ ---
 WIDTH, HEIGHT = 800, 600
 init()
+mixer.init()
 screen = display.set_mode((WIDTH, HEIGHT))
 clock = time.Clock()
 display.set_caption("Пінг-Понг")
+
 # ---СЕРВЕР ---
 def connect_to_server():
     while True:
@@ -40,7 +42,6 @@ def receive():
 # --- ШРИФТИ ---
 font_win = font.Font(None, 72)
 font_main = font.Font(None, 36)
-
 # --- ЗОБРАЖЕННЯ ----
 fon_game = transform.scale(image.load('images/ChatGPT Image 1 серп. 2026 р., 16_40_15.png'), (WIDTH, HEIGHT))
 fon_win  = transform.scale(image.load('images/images.jfif'), (WIDTH, HEIGHT))
@@ -49,6 +50,11 @@ fon_load = transform.scale(image.load('images/ChatGPT Image 1 серп. 2026 р.
 bullet_image = transform.scale(image.load('images/ТРАМП_КРУГ.png'), (20, 20))
 
 # --- ЗВУКИ ---
+mixer.music.load('music/hurryup.wav')
+mixer.music.play(-1)
+
+ball_touch = mixer.Sound('music/FX01.wav')
+win = mixer.Sound('music/Coin01.wav')
 
 # --- ГРА ---
 game_over = False
@@ -90,6 +96,8 @@ while True:
         text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 120))
         screen.blit(text, text_rect)
 
+        win.play()
+
         display.update()
         continue  # Блокує гру після перемоги
 
@@ -102,12 +110,8 @@ while True:
         screen.blit(score_text, (WIDTH // 2 -25, 20))
 
         if game_state['sound_event']:
-            if game_state['sound_event'] == 'wall_hit':
-                # звук відбиття м'ячика від стін
-                pass
             if game_state['sound_event'] == 'platform_hit':
-                # звук відбиття м'ячика від платформи
-                pass
+                ball_touch.play()
 
     else:
         wating_text = font_main.render(f"Очікування гравців...", True, (255, 255, 255))
